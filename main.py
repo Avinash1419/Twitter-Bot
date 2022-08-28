bf55e2ba2992d60b40dab332a6f52df4b12084ad
pranithk91 <pranithk91@gmail.com>
2022-08-28 18:30:20 +0530

    Pushing new code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sys import api_version
 import tweepy
+import time
+
 token_file_name = 'Tokens_kp.txt'  #Add your file name
 all_keys = open(token_file_name, 'r').read().splitlines()
 
@@ -12,5 +14,8 @@ authenticator = tweepy.OAuthHandler(api_key,api_secret)
 authenticator.set_access_token(access_token,access_secret)
 
 api = tweepy.API(authenticator, wait_on_rate_limit= True)
+time.sleep(600)
+#api.create_friendship(screen_name = 'AshTee42')
+#api.update_status('There must be a reason why I am posting a tweet for the first time! #randomthought')
+api.retweet(1363352214559891456)
 
-api.create_friendship( 'Avinash Thavva', 'AshTee42')
